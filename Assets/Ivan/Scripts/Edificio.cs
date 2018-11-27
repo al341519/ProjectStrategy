72bdc9db284c5ef5cd528488726816bb5260d30e
Ivanlol97 <33065563+Ivanlol97@users.noreply.github.com>
2018-11-27 01:40:44 +0100

    Edificios
    Mi primer Commit :D

---
+++ Assets/Ivan/Scripts/Edificio.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edificio : MonoBehaviour {
+
+    int vida;
+    int madera;
+    int trigo;
+    int metal;
+    int aldeano;
+
+    
+    string id;
+
+    GameManager game;
+    int[] recursos;
+
+    public GameObject aserraderoPrefab;
+    public GameObject forjaPrefab;
+    public GameObject molinoPrefab;
+    public GameObject infanteriaPrefab;
+    public GameObject arqueriaPrefab;
+    public GameObject caballeriaPrefab;
+    public GameObject castilloPrefab;
+
+    public void Start()
+    {
+        game = GetComponent<GameManager>();
+
+        recursos = game.GetRecursos();
+    }
+
+    public Edificio(string identificador, int x, int y)
+    {
+        transform.position = new Vector3 (x, 0, y);
+        id = identificador;
+
+        switch (id)
+        {
+            case "aserradero":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "forja":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "molino":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "infantería":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "arquería":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "caballería":
+                vida = 50;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 1;
+                break;
+            case "castillo":
+                vida = 100;
+                madera = 0;
+                trigo = 0;
+                metal = 0;
+                aldeano = 0;
+                break;
+            default:
+                break;
+        }
+
+        crearEdificio();
+    }
+
+    void crearEdificio()
+    {
+
+        if (recursos[0] >= madera &&
+            recursos[1] >= trigo &&
+            recursos[2] >= metal &&
+            recursos[3] >= aldeano)
+        {
+
+            game.UpdateRecursos(madera, trigo, metal, aldeano);
+
+            switch (id)
+            {
+                case "aserradero":
+                    Instantiate(aserraderoPrefab);
+                    break;
+                case "forja":
+                    Instantiate(forjaPrefab);
+                    break;
+                case "molino":
+                    Instantiate(molinoPrefab);
+                    break;
+                case "infantería":
+                    Instantiate(infanteriaPrefab);
+                    break;
+                case "arquería":
+                    Instantiate(arqueriaPrefab);
+                    break;
+                case "caballería":
+                    Instantiate(caballeriaPrefab);
+                    break;
+                case "castillo":
+                    Instantiate(castilloPrefab);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        else
+        {
+            Debug.Log("NO HAY SUFICIENTES MATERIALES");
+        }
+    }
+}
---
+++ Assets/Ivan/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    int[] recursos;
+
+    HexCell cell;
+
+    int madera;
+    int trigo;
+    int metal;
+    int aldeanos;
+    int vida;
+
+    Edificio edificio;
+    int x;
+    int y;
+
+    public Texture2D IconContainer;
+    public Texture2D iconMine;
+    public Texture2D iconHoverMine;
+    public Texture2D iconCastle;
+    public Texture2D iconHoverCastle;
+
+    private void Awake()
+    {
+        recursos = new int [4];
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle container = new GUIStyle();
+        container.normal.background = IconContainer;
+
+        GUI.Box(new Rect(Screen.width / 2 - 200, Screen.height - 40, 400, 50), "", container);
+
+        //Generate unit buttons
+        int offset = 48;
+        int j = 0;
+        string id;
+        
+
+        for(int i = 0; i < 2; i++)
+        {
+            GUIStyle Icon = new GUIStyle();
+            if (i == 0)
+            {
+                id = "forja";
+                x = -3;
+                y = 6;
+                Icon.normal.background = iconMine;
+                Icon.hover.background = iconHoverMine;
+            }
+            else
+            {
+                id = "castillo";
+                x = 0;
+                y = 0;
+                Icon.normal.background = iconCastle;
+                Icon.hover.background = iconHoverCastle;
+            }
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 199 + (offset * j), Screen.height - 39, 46, 39), "", Icon))
+            {
+                Debug.Log("Pulsado");
+
+                //CREAR EDIFICIO
+                
+                edificio = new Edificio(id, x, y);
+            }
+
+            j++;
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log( Time.time);
+    }
+
+    public int[] GetRecursos()
+    {
+        return recursos;
+    }
+
+    public void UpdateRecursos(int madera, int trigo, int metal, int aldeano)
+    {
+        recursos[0] -= madera;
+        recursos[1] -= trigo;
+        recursos[2] -= metal;
+        recursos[3] -= aldeano;
+    }
+
+}
