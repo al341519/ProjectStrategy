dd7d6863a4a2f2dbd86e50bb6240c276eb959a25
al341552 <33596891+al341552@users.noreply.github.com>
2018-12-13 23:09:29 +0100

    Scripts animaciones

--- Assets/Scripts/InfantryClass.cs
+++ Assets/Scripts/InfantryClass.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : UnityClass {
+public class InfantryClass : UnityClass {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/VillagerClass.cs
+++ Assets/Scripts/VillagerClass.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MilitiaClass : UnityClass {
+public class VillagerClass : UnityClass {
 
 	// Use this for initialization
 	void Start () {
