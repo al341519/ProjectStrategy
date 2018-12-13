fe6b7c0ce453aa191835bc21ee9a7f0cf6c88d5d
yuguui <33630524+yuguui@users.noreply.github.com>
2018-12-13 22:48:29 +0100

    test

--- Assets/Ivan/Scripts/UIManager.cs
+++ Assets/Ivan/Scripts/UIManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager {
+public class UIManager:MonoBehaviour {
 
     public GameObject UIInicio;
     public GameObject UIRecursos;
